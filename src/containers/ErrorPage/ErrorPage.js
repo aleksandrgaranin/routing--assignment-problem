2f962d9ad82967161bc3fe1cf97aa52c81bea0ee
aleksandGG <aleksandrgaranin89@gmail.com>
2020-09-03 13:10:11 -0700

    first commit

---
+++ src/containers/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const errorPage = () => {
+    return (
+        <div>
+            <p>404 Error page</p>
+            <p>Time to call for Help</p>
+        </div>
+    );
+}
+
+export default errorPage;
